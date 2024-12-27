a90ab9006fa6da728961732b9d16728087d344b1
Minli Liao <ml2076@cam.ac.uk>
2024-12-27 06:13:13 +0000

    Script and makefile to get SPEC06 compiling with nZDC (SPEC06 image not included)

---
+++ setup_spec06.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SPEC06_DIR=${SCRIPT_DIR}/spec06
+BENCH_DIR=${SCRIPT_DIR}/benchmarks
+
+# Get SPEC06
+cd ${SCRIPT_DIR}
+# mkdir specmnt
+# mkdir ${SPEC06_DIR}
+# sudo mount -o loop *.iso specmnt
+# cd ${SPEC06_DIR}
+# tar -xJvf ${SCRIPT_DIR}/specmnt/install_archives/cpu2006.tar.xz
+# cd ${SCRIPT_DIR}
+# sudo umount specmnt
+# rm -r specmnt
+
+# Setup source and run folders
+cd ${BENCH_DIR}
+for bench in 401.bzip2 429.mcf 456.hmmer 458.sjeng 462.libquantum 473.astar 400.perlbench 445.gobmk 464.h264ref 403.gcc 471.omnetpp 483.xalancbmk; do
+    cp -r ${SPEC06_DIR}/benchspec/CPU2006/${bench}/src ${BENCH_DIR}/${bench}
+    cp ${BENCH_DIR}/${bench}/Makefile* ${BENCH_DIR}/${bench}/src
+    mkdir -p ${BENCH_DIR}/${bench}/run
+    if [ -e ${SPEC06_DIR}/benchspec/CPU2006/${bench}/data/all/input ]; then
+        cp -r ${SPEC06_DIR}/benchspec/CPU2006/${bench}/data/all/input ${BENCH_DIR}/${bench}/run
+        mv ${BENCH_DIR}/${bench}/run/input/* ${BENCH_DIR}/${bench}/run
+        rm -r ${BENCH_DIR}/${bench}/run/input
+    fi
+    cp -r ${SPEC06_DIR}/benchspec/CPU2006/${bench}/data/ref/input ${BENCH_DIR}/${bench}/run
+    mv ${BENCH_DIR}/${bench}/run/input/* ${BENCH_DIR}/${bench}/run
+    rm -r ${BENCH_DIR}/${bench}/run/input
+done
